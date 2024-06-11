425c9531019d08656aaa7d35496e9cd63f335b27
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2024-06-12 00:59:14 +0200

    add comments

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+// display the squares in the container (dimension is number of row/columns)
+
 const container = document.querySelector("#container");
 
 const addDiv = (dimension) => {
-    const height = 400/dimension;
+    //create Rows
     for (i=1; i<dimension +1; i++){
         const divRow = document.createElement("div");
         divRow.classList.add(`row`)
         divRow.setAttribute("style", "flex: 1; align-content: stretch; ")
+        //fill rows with divs
         for (j=1; j < dimension+1; j++) {
             const divRowElement = document.createElement("div");
             divRowElement.classList.add("divRowElement");
-            divRowElement.setAttribute("style", "display; border: 1px solid black; ")
+            divRowElement.setAttribute("style", "display; aspect-ratio: 1/1; border: 1px solid black; ")
             divRow.appendChild(divRowElement)
         }
         container.appendChild(divRow)
